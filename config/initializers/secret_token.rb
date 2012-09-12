670a5e3d2f5a30be832a527f1fe104194b712587
Elisa Cutrin <elisacutrin@gmail.com>
2012-09-11 21:23:27 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BucketList::Application.config.secret_token = '7bfd58047fdf53c3ac2bd0e36f9521ce3954147e52cff6c9caf06974a0906b12b7af8a9b69a889eed48c904a8cbf98f7ec63f6d9256f6b1d3324c11f71e2f5b2'
